466d0d8c60049013500c265ad389273e9a97b8a1
shafayet-hossain <shafayethossaing4609@gmail.com>
2022-03-26 15:16:58 +0600

    added sizeof

--- array.c
+++ array.c
@@ -3,9 +3,10 @@ int main()
 {
     int myArr[3] = {1,2,3};
     int i;
+    int n = sizeof(myArr)/sizeof(myArr[0]);
 
-    for(i = 0; i<3; i++){
-        printf("%d", myArr[i]);
+    for(i = 0; i<n; i++){
+        printf("myArr[%d] = %d\r\n",i, myArr[i]);
     }
     return 0;
 }
